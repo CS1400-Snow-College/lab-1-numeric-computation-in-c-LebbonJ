dc87118b9cca043758d0865f3cd2744fa9171ab9
LebbonJ <justin.lebbon@students.snow.edu>
2025-01-21 18:47:08 -0700

    Sixth Commit

--- Program.cs
+++ Program.cs
@@ -5,3 +5,10 @@ int d = (a + b) / c;
 int e = (a + b) % c;
 Console.WriteLine($"quotient: {d}");
 Console.WriteLine($"remainder: {e}");
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");
