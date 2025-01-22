3afeb8871a88e7503bd9cc37306a1309733efa55
LebbonJ <justin.lebbon@students.snow.edu>
2025-01-21 18:40:51 -0700

    Fifth Commit

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿int a = 5;
+﻿int a = 7;
 int b = 4;
-int c = 2;
-int d = (a + b) - 6 * c + (12*4) / 3 + 12;
-Console.WriteLine (d);
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
